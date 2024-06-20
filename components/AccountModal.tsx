472f6dc7f7a502da3b1f6a8763e670a24cc662eb
Damith08 <dmthsamarasinghe@gmail.com>
2024-06-20 20:38:01 +0530

    feat: create modal ui

---
+++ components/AccountModal.tsx
@@ -0,0 +1,168 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Modal,
+  Pressable,
+} from 'react-native';
+import React, {useState} from 'react';
+
+const AccountModal = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  return (
+    <>
+      <Modal
+        animationType="fade"
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}>
+        <View>
+          <View>
+            {/* <AccountModal /> */}
+            <View style={styles.card}>
+              <View style={styles.cardContent}>
+                <View style={styles.header}>
+                  <TouchableOpacity>
+                    <Text>EatMe Logo</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    onPress={() => setModalVisible(!modalVisible)}>
+                    <Text>X</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+            <View style={styles.container}>
+              <TouchableOpacity style={styles.signupOrLoginButton}>
+                <Text style={styles.buttonText}>Sign up or log in </Text>
+              </TouchableOpacity>
+            </View>
+            <View>
+              <TouchableOpacity style={styles.optionTab}>
+                <View style={styles.cardContent}>
+                  <View style={styles.optionRow}>
+                    <View style={styles.iconNText}>
+                      <Text>i</Text>
+                      <Text>Become a Rider</Text>
+                    </View>
+                    <View>
+                      <Text>D</Text>
+                    </View>
+                  </View>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.optionTab}>
+                <View style={styles.cardContent}>
+                  <View style={styles.optionRow}>
+                    <View style={styles.iconNText}>
+                      <Text>i</Text>
+                      <Text>Add your restaurant or store</Text>
+                    </View>
+                    <View>
+                      <Text>D</Text>
+                    </View>
+                  </View>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.optionTab}>
+                <View style={styles.cardContent}>
+                  <View style={styles.optionRow}>
+                    <View style={styles.iconNText}>
+                      <Text>i</Text>
+                      <Text>Sign up your office</Text>
+                    </View>
+                    <View>
+                      <Text>D</Text>
+                    </View>
+                  </View>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.optionTab}>
+                <View style={styles.cardContent}>
+                  <View style={styles.optionRow}>
+                    <View style={styles.iconNText}>
+                      <Text>i</Text>
+                      <Text>FAQs</Text>
+                    </View>
+                    <View>
+                      <Text>D</Text>
+                    </View>
+                  </View>
+                </View>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
+      <Pressable onPress={() => setModalVisible(true)}>
+        <Text>Account</Text>
+      </Pressable>
+    </>
+  );
+};
+
+export default AccountModal;
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 20,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  card: {
+    backgroundColor: 'white',
+    padding: 5,
+    marginBottom: 10,
+    shadowColor: '#333',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  },
+  cardContent: {
+    marginHorizontal: 10,
+    marginVertical: 10,
+  },
+  signupOrLoginButton: {
+    backgroundColor: '#E27A39',
+    padding: 15,
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '900',
+  },
+  optionRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  iconNText: {
+    flexDirection: 'row',
+  },
+  optionTab: {
+    backgroundColor: 'white',
+    padding: 5,
+    // marginBottom: 10,
+    shadowColor: '#333',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  },
+});
