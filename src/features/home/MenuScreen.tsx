bf856afe378f6ff97ff299bbccd93ae9c1adda67
Damith08 <dmthsamarasinghe@gmail.com>
2024-06-23 21:16:42 +0530

    feat: add menu screen

---
+++ src/features/home/MenuScreen.tsx
@@ -0,0 +1,16 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+
+const MenuScreen = () => {
+  return (
+    <View>
+      <Header />
+      <Text>Menu</Text>
+      <Footer />
+    </View>
+  );
+};
+
+export default MenuScreen;
