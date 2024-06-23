c9cbeefc3995131189821a74b4f5007ff73d2500
Damith08 <dmthsamarasinghe@gmail.com>
2024-06-23 21:14:58 +0530

    feat: create svg component

--- src/theme/Svgs.ts
+++ src/theme/Svgs.ts
@@ -1,5 +1,31 @@
 import Logo from '../assets/svgs/logo-text.svg';
+import Account from '../assets/svgs/account.svg';
+import Home from '../assets/svgs/home.svg';
+import Send from '../assets/svgs/send.svg';
+import AppStore from '../assets/svgs/app-store.svg';
+import PlayStore from '../assets/svgs/google-play-store.svg';
+import AppleStore from '../assets/svgs/app-store-badge.svg';
+import Facebook from '../assets/svgs/facebook.svg';
+import Instagram from '../assets/svgs/instagram.svg';
+import Twitter from '../assets/svgs/twitter.svg';
+import MerchItemRow from '../assets/svgs/merch-itemrow.svg';
+import Gmail from '../assets/svgs/gmail.svg';
+import Apple from '../assets/svgs/apple.svg';
+import Email from '../assets/svgs/email.svg';
 
 export const Svgs = {
   Logo,
+  Account,
+  Home,
+  Send,
+  AppStore,
+  PlayStore,
+  AppleStore,
+  Facebook,
+  Instagram,
+  Twitter,
+  MerchItemRow,
+  Gmail,
+  Apple,
+  Email,
 };
