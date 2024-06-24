d882376d3a5f4bcd35706b43cc660364da061603
Damith08 <dmthsamarasinghe@gmail.com>
2024-06-24 23:39:47 +0530

    perf: use array map method to avoid code duplication in landing screen component

---
+++ src/constants/CardComponent.ts
@@ -0,0 +1,38 @@
+export const CardComponent = [
+  {
+    title: 'Partner with us',
+    description:
+      'Join Deliveroo and reach more customers than ever. We handle delivery, so you can focus on the food.',
+    source: {
+      uri: 'https://img.freepik.com/free-photo/side-view-cook-making-delicious-pasta_23-2150690631.jpg',
+    },
+    onPress: () => {},
+  },
+  {
+    title: 'Ride with us',
+    description:
+      'The freedom to fit work around your life. Plus great fees, perks and discounts.',
+    source: {
+      uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfocaXNLEB63uQ85fjN5cuok3EX7zByhv-kvflL15GWKZfHYBkDBprzIr9pGgbp0zgFIM&usqp=CAU',
+    },
+    onPress: () => {},
+  },
+  {
+    title: 'EatMe for Work',
+    description:
+      "From team lunches to meal allowances for your late night workers, we've got your workplace meals covered.",
+    source: {
+      uri: 'https://www.roadie.com/wp-content/uploads/2024/04/HomeDelivery.jpeg',
+    },
+    onPress: () => {},
+  },
+  {
+    title: 'Gift Cards',
+    description:
+      'Looking for an easy way to treat your friends and family? Give the gift of great food with a Deliveroo gift card.',
+    source: {
+      uri: 'https://nypost.com/wp-content/uploads/sites/2/2023/11/gift-card.jpg?quality=75&strip=all&w=1024',
+    },
+    onPress: () => {},
+  },
+];
--- src/features/home/LandingScreen.tsx
+++ src/features/home/LandingScreen.tsx
@@ -16,6 +16,7 @@ import {ROOT_STACK_SCREENS} from '../../constants/NavigationConstants';
 import DetailCard from '../../components/DetailCard';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {RootStackParams} from '../../navigation/RootStackNavigator';
+import {CardComponent} from '../../constants/CardComponent';
 
 const LandingScreen = () => {
   const navigation =
@@ -109,38 +110,15 @@ const LandingScreen = () => {
           <Svgs.MerchItemRow height={70} width={600} />
         </View>
       </View>
-      <DetailCard
-        title="Partner with us"
-        description="Join Deliveroo and reach more customers than ever. We handle delivery, so you can focus on the food."
-        source={{
-          uri: 'https://img.freepik.com/free-photo/side-view-cook-making-delicious-pasta_23-2150690631.jpg',
-        }}
-        onPress={() => {}}
-      />
-      <DetailCard
-        title="Ride with us"
-        description="The freedom to fit work around your life. Plus great fees, perks and discounts."
-        source={{
-          uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfocaXNLEB63uQ85fjN5cuok3EX7zByhv-kvflL15GWKZfHYBkDBprzIr9pGgbp0zgFIM&usqp=CAU',
-        }}
-        onPress={() => {}}
-      />
-      <DetailCard
-        title="EatMe for Work"
-        description="From team lunches to meal allowances for your late night workers, we've got your workplace meals covered."
-        source={{
-          uri: 'https://www.roadie.com/wp-content/uploads/2024/04/HomeDelivery.jpeg',
-        }}
-        onPress={() => {}}
-      />
-      <DetailCard
-        title="Gift Cards"
-        description="Looking for an easy way to treat your friends and family? Give the gift of great food with a Deliveroo gift card."
-        source={{
-          uri: 'https://nypost.com/wp-content/uploads/sites/2/2023/11/gift-card.jpg?quality=75&strip=all&w=1024',
-        }}
-        onPress={() => {}}
-      />
+      {CardComponent.map(item => (
+        <DetailCard
+          title={item.title}
+          description={item.description}
+          source={item.source}
+          onPress={item.onPress}
+          key={item.title}
+        />
+      ))}
       <Footer />
     </ScrollView>
   );
