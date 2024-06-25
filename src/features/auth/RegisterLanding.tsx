a8e66daa884d76b04d9bf6f7423e4d3fd2ba13ed
Damith08 <dmthsamarasinghe@gmail.com>
2024-06-25 17:36:46 +0530

    feat: registaration validation fixed

--- src/features/auth/RegisterLanding.tsx
+++ src/features/auth/RegisterLanding.tsx
@@ -13,14 +13,31 @@ import {useNavigation} from '@react-navigation/native';
 import {ROOT_STACK_SCREENS} from '../../constants/NavigationConstants';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {RootStackParams} from '../../navigation/RootStackNavigator';
+import {useForm, Controller, SubmitHandler} from 'react-hook-form';
+import {RegisterFormData} from '../../types/types';
 
 const RegisterLanding = (): React.JSX.Element => {
   const navigation =
     useNavigation<NativeStackNavigationProp<RootStackParams>>();
 
   const menuScreenLandingHandler = () => {
+    handleSubmit(onSubmit)();
+  };
+
+  const {
+    control,
+    handleSubmit,
+    formState: {errors},
+  } = useForm<RegisterFormData>({
+    defaultValues: {
+      firstName: '',
+    },
+  });
+  const onSubmit: SubmitHandler<RegisterFormData> = data => {
+    console.log(data);
     navigation.navigate(ROOT_STACK_SCREENS.MENU_SCREEN);
   };
+
   return (
     <ScrollView>
       <View style={styles.headerContainer}>
@@ -28,21 +45,186 @@ const RegisterLanding = (): React.JSX.Element => {
       </View>
       <View style={styles.container}>
         <Text style={styles.textHeading}>Sign in</Text>
-        <Text>Full Name</Text>
-        <View style={styles.textInputContainer}>
-          <TextInput style={styles.textInput} placeholder="e.g. John Doe" />
-        </View>
-        <Text>Email</Text>
-        <View style={styles.textInputContainer}>
-          <TextInput
-            style={styles.textInput}
-            placeholder="e.g.name@example.com"
-          />
-        </View>
-        <Text>Password</Text>
-        <View style={styles.textInputContainer}>
-          <TextInput style={styles.textInput} placeholder="e.g. John Doe" />
-        </View>
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({field: {onChange, onBlur, value}}) => (
+            <>
+              <Text>First Name</Text>
+              <View
+                style={[
+                  styles.textInputContainer,
+                  errors.firstName && styles.errorInput,
+                ]}>
+                <TextInput
+                  style={styles.textInput}
+                  placeholder="e.g. John"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                  placeholderTextColor="#8f949d"
+                />
+              </View>
+            </>
+          )}
+          name="firstName"
+        />
+        {errors.firstName && (
+          <Text style={styles.errorText}>First Name is required.</Text>
+        )}
+
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({field: {onChange, onBlur, value}}) => (
+            <>
+              <Text>Surname</Text>
+              <View
+                style={[
+                  styles.textInputContainer,
+                  errors.firstName && styles.errorInput,
+                ]}>
+                <TextInput
+                  style={styles.textInput}
+                  placeholder="e.g. Doe"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                  placeholderTextColor="#8f949d"
+                />
+              </View>
+            </>
+          )}
+          name="surname"
+        />
+        {errors.surname && (
+          <Text style={styles.errorText}>Surname is required.</Text>
+        )}
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({field: {onChange, onBlur, value}}) => (
+            <>
+              <Text>Location</Text>
+              <View
+                style={[
+                  styles.textInputContainer,
+                  errors.firstName && styles.errorInput,
+                ]}>
+                <TextInput
+                  style={styles.textInput}
+                  placeholder="e.g. Colombo"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                  placeholderTextColor="#8f949d"
+                />
+              </View>
+            </>
+          )}
+          name="location"
+        />
+        {errors.location && (
+          <Text style={styles.errorText}>Location is required.</Text>
+        )}
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({field: {onChange, onBlur, value}}) => (
+            <>
+              <Text>Mobile Number</Text>
+              <View
+                style={[
+                  styles.textInputContainer,
+                  errors.firstName && styles.errorInput,
+                ]}>
+                <TextInput
+                  style={styles.textInput}
+                  placeholder="e.g. +94775983761"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                  keyboardType="phone-pad"
+                  placeholderTextColor="#8f949d"
+                />
+              </View>
+            </>
+          )}
+          name="mobile"
+        />
+        {errors.mobile && (
+          <Text style={styles.errorText}>Mobile Number is required.</Text>
+        )}
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({field: {onChange, onBlur, value}}) => (
+            <>
+              <Text>Email</Text>
+              <View
+                style={[
+                  styles.textInputContainer,
+                  errors.firstName && styles.errorInput,
+                ]}>
+                <TextInput
+                  style={styles.textInput}
+                  placeholder="e.g. example@example.com"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                  placeholderTextColor="#8f949d"
+                  keyboardType="email-address"
+                  autoComplete="email"
+                />
+              </View>
+            </>
+          )}
+          name="email"
+        />
+        {errors.email && (
+          <Text style={styles.errorText}>Mobile Number is required.</Text>
+        )}
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({field: {onChange, onBlur, value}}) => (
+            <>
+              <Text>Password</Text>
+              <View
+                style={[
+                  styles.textInputContainer,
+                  errors.firstName && styles.errorInput,
+                ]}>
+                <TextInput
+                  style={styles.textInput}
+                  placeholder="password"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                  placeholderTextColor="#8f949d"
+                  textContentType="password"
+                  secureTextEntry={true}
+                />
+              </View>
+            </>
+          )}
+          name="password"
+        />
+        {errors.password && (
+          <Text style={styles.errorText}>Password is required.</Text>
+        )}
         <TouchableOpacity
           style={styles.continueButton}
           onPress={menuScreenLandingHandler}>
@@ -94,5 +276,12 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 15,
     fontWeight: 'bold',
+  },
+  errorText: {
+    color: '#900',
+  },
+  errorInput: {
+    borderWidth: 0.75,
+    borderColor: '#900',
   },
 });
---
+++ src/types/types.ts
@@ -0,0 +1,10 @@
+export type RegisterFormData = {
+  firstName: string;
+  middleName: string;
+  surname: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  location: string;
+  mobile: string;
+};
