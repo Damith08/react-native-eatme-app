183ab4d9a3248f06f9f0ecb4f40100b55c7e4cbc
Damith08 <dmthsamarasinghe@gmail.com>
2024-06-27 12:39:21 +0530

    feat: create types for Registartion and Login forms

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,10 +1,13 @@
 export type RegisterFormData = {
   firstName: string;
-  middleName: string;
   surname: string;
   email: string;
   password: string;
-  confirmPassword: string;
-  location: string;
+  address: string;
   mobile: string;
+};
+
+export type LoginFormData = {
+  email: string;
+  password: string;
 };
