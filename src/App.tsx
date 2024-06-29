cbe682aab30d3e984a8fcd34a15d342c641c5cc5
Damith08 <dmthsamarasinghe@gmail.com>
2024-06-29 16:55:27 +0530

    feat: add store to the app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,15 +9,17 @@ import React from 'react';
 import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import RootStackNavigator from './navigation/RootStackNavigator';
+import {Provider} from 'react-redux';
+import store from './store/store';
 
 function App(): React.JSX.Element {
   return (
-    <>
+    <Provider store={store}>
       <StatusBar barStyle="light-content" />
       <NavigationContainer>
         <RootStackNavigator />
       </NavigationContainer>
-    </>
+    </Provider>
   );
 }
 
