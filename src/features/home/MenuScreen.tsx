9b02305d259d76d62aec4b18319280a152032b00
Damith08 <dmthsamarasinghe@gmail.com>
2024-06-30 19:52:51 +0530

    feat: add restaurant details to the menu screen

--- src/features/home/MenuScreen.tsx
+++ src/features/home/MenuScreen.tsx
@@ -1,13 +1,14 @@
-import {Text, View} from 'react-native';
+import {View} from 'react-native';
 import React from 'react';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+import MenuRestaurantCard from '../../components/MenuRestaurantCard';
 
 const MenuScreen = () => {
   return (
     <View>
       <Header />
-      <Text>Menu</Text>
+      <MenuRestaurantCard />
       <Footer />
     </View>
   );
