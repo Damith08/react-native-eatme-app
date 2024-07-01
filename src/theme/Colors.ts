9360af5b91ef14e8fe51b2750f4a0569216b4cc5
Damith08 <dmthsamarasinghe@gmail.com>
2024-07-01 18:23:57 +0530

    style: add colors

---
+++ src/theme/Colors.ts
@@ -0,0 +1,10 @@
+export const Colors = {
+  eatMeColor: '#E27A39',
+  success: '#4d7c1b',
+  error: '#900',
+  disableButtonColor: '#d3d3d3',
+  disableButtonTextColor: '#313640',
+  placeholderTextColor: '#8f949d',
+  shadowColor: '#333',
+  backgroundPrimary: '#FFFFFF',
+};
