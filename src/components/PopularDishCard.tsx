96016c8c49c7588efe8d31c3746b28bf952bbb90
Damith08 <dmthsamarasinghe@gmail.com>
2024-07-02 11:46:21 +0530

    feat: create popular dish card component

---
+++ src/components/PopularDishCard.tsx
@@ -0,0 +1,66 @@
+import {View, Text, StyleSheet, Image} from 'react-native';
+import React from 'react';
+import {Colors} from '../theme/Colors';
+import AddButton from './AddButton';
+import {Images} from '../theme/Images';
+
+const PopularDishCard = () => {
+  return (
+    <View style={styles.rootContainer}>
+      <View style={styles.card}>
+        <Image
+          height={100}
+          width={110}
+          source={{
+            uri: Images.dishImage,
+          }}
+        />
+        <View style={styles.cardContent}>
+          <Text style={styles.dishName}>Dish Name</Text>
+          <View style={styles.dishDescriptionContainer}>
+            <Text>kcal</Text>
+            <Text>Price</Text>
+          </View>
+          <AddButton />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default PopularDishCard;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    marginTop: 10,
+    marginBottom: 10,
+    marginRight: 20,
+  },
+  card: {
+    backgroundColor: Colors.backgroundPrimary,
+    borderRadius: 5,
+    marginBottom: 10,
+    shadowColor: Colors.shadowColor,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  },
+  cardContent: {
+    marginTop: 6,
+    marginLeft: 7,
+    marginRight: 7,
+    marginBottom: 5,
+  },
+  dishName: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  dishDescriptionContainer: {
+    marginTop: 2,
+  },
+});
--- src/theme/Images.ts
+++ src/theme/Images.ts
@@ -1,4 +1,6 @@
 export const Images = {
   mapImage:
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt85bgXe0z4wUGVk_a0-8AvNOf1fa1RVbUzg&s',
+  dishImage:
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTT3_9m2PP7d4QSNMk56kYOsb_Ue8k3-pFmIg&s',
 };
