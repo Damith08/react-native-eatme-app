7de9bc45c4ac7fa7b6368179a37cb35cf173c9dd
Damith08 <dmthsamarasinghe@gmail.com>
2024-07-02 18:01:48 +0530

    feat: add  restaurant card, dishes and popular dishes cards

--- src/features/home/MenuScreen.tsx
+++ src/features/home/MenuScreen.tsx
@@ -1,18 +1,22 @@
-import {View} from 'react-native';
+import {ScrollView} from 'react-native';
 import React from 'react';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import MenuRestaurantCard from '../../components/MenuRestaurantCard';
 import PopularDishCategory from '../../components/PopularDishCategory';
+import RestaurantDishesCategory from '../../components/RestaurantDishesCategory';
 
 const MenuScreen = () => {
   return (
-    <View>
+    <ScrollView>
       <Header />
       <MenuRestaurantCard />
       <PopularDishCategory />
+      <RestaurantDishesCategory />
+      <RestaurantDishesCategory />
+      <RestaurantDishesCategory />
       <Footer />
-    </View>
+    </ScrollView>
   );
 };
 
