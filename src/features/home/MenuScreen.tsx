4e1aff4978a6309ff1a719c21749f5f5f7a392ee
Damith08 <dmthsamarasinghe@gmail.com>
2024-07-04 11:46:57 +0530

    feat: fix menu screen

--- src/features/home/MenuScreen.tsx
+++ src/features/home/MenuScreen.tsx
@@ -1,10 +1,12 @@
-import {ScrollView} from 'react-native';
+import {FlatList, ScrollView} from 'react-native';
 import React from 'react';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import MenuRestaurantCard from '../../components/MenuRestaurantCard';
 import PopularDishCategory from '../../components/PopularDishCategory';
 import RestaurantDishesCategory from '../../components/RestaurantDishesCategory';
+import {RESTAURANT_MENU_CATEGORY_DATA} from '../../data/data';
+import {View} from 'react-native-ui-lib';
 
 const MenuScreen = () => {
   return (
@@ -12,10 +14,20 @@ const MenuScreen = () => {
       <Header />
       <MenuRestaurantCard />
       <PopularDishCategory />
-      <RestaurantDishesCategory />
-      <RestaurantDishesCategory />
-      <RestaurantDishesCategory />
-      <Footer />
+      <View>
+        <FlatList
+          scrollEnabled={false}
+          data={RESTAURANT_MENU_CATEGORY_DATA}
+          keyExtractor={item => item.id.toString()}
+          renderItem={({item}) => (
+            <RestaurantDishesCategory
+              categoryName={item.categoryName}
+              dishes={item.dishes}
+            />
+          )}
+        />
+        <Footer />
+      </View>
     </ScrollView>
   );
 };
