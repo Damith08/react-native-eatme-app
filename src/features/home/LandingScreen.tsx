2973c94d7f10d495a68e1e88cd6e930851f4c39f
Damith08 <dmthsamarasinghe@gmail.com>
2024-07-12 15:45:17 +0530

    feat: add a section for upper part of the landing screen
    in landing page add partners logo banner  move the component that containe upper part of the screen to a new component and ad a background image

--- src/features/home/LandingScreen.tsx
+++ src/features/home/LandingScreen.tsx
@@ -1,84 +1,30 @@
 import React from 'react';
-import HeaderComponent from '../../components/Header';
+
 import {
   Image,
+  ImageBackground,
   ScrollView,
   StyleSheet,
-  Text,
-  TextInput,
   TouchableOpacity,
   View,
 } from 'react-native';
 import Footer from '../../components/Footer';
 import {Svgs} from '../../theme/Svgs';
-import {useNavigation} from '@react-navigation/native';
-import {ROOT_STACK_SCREENS} from '../../constants/NavigationConstants';
 import DetailCard from '../../components/DetailCard';
-import {NativeStackNavigationProp} from '@react-navigation/native-stack';
-import {RootStackParams} from '../../navigation/RootStackNavigator';
 import {CardComponent} from '../../constants/CardComponent';
 import {Colors} from '../../theme/Colors';
 import {Images} from '../../theme/Images';
 import AppText from '../../components/AppText';
 import {FontFamily} from '../../theme/FontFamily';
-import {FontWeight} from '../../theme/FontWeight';
+import SectionDetail from '../../components/SectionDetail';
 
 const LandingScreen = () => {
-  const navigation =
-    useNavigation<NativeStackNavigationProp<RootStackParams>>();
-  const authLandingHandler = () => {
-    navigation.navigate(ROOT_STACK_SCREENS.AUTH);
-  };
-
   return (
     <ScrollView>
-      <HeaderComponent />
-      <View style={styles.container}>
-        <AppText
-          fontFamily={FontFamily.BOLD}
-          fontSize={38}
-          weight={FontWeight.bold}
-          text="Restaurant food, takeaway and groceries. Delivered"
-        />
-      </View>
-      <View style={styles.container}>
-        <View style={styles.card}>
-          <View style={styles.cardContent}>
-            <AppText
-              text="Enter a postcode to see what we deliver:"
-              fontFamily={FontFamily.REGULAR}
-              fontSize={14}
-            />
-            <View style={styles.searchBar}>
-              <View style={styles.searchButtonComponent}>
-                <View style={styles.sendIcon}>
-                  <Svgs.Send height={20} width={20} fill={Colors.eatMeColor} />
-                </View>
-                <TextInput
-                  style={styles.inputText}
-                  placeholder="e.g. EC4R 3TE"
-                  placeholderTextColor={Colors.placeholderTextColor}
-                />
-                <TouchableOpacity style={styles.searchButton}>
-                  <Text style={styles.searchButtonText}>Search</Text>
-                </TouchableOpacity>
-              </View>
-            </View>
-            <View style={styles.searchComponentText2}>
-              <TouchableOpacity onPress={authLandingHandler}>
-                <AppText
-                  text="Log in"
-                  color={Colors.eatMeColor}
-                  fontSize={14}
-                />
-              </TouchableOpacity>
-              <AppText text=" for your recent addresses" fontSize={14} />
-            </View>
-          </View>
-        </View>
-      </View>
-      <View style={styles.container}>
-        <Text>Photos banner</Text>
+      <SectionDetail />
+
+      <View style={styles.partnersImageContainer}>
+        <Image source={{uri: Images.partnersImages}} height={200} width={500} />
       </View>
       <View style={styles.container}>
         <View style={styles.card}>
@@ -87,12 +33,14 @@ const LandingScreen = () => {
               text="Track orders to your door"
               fontFamily={FontFamily.BOLD}
               fontSize={34}
+              color={Colors.textColor}
             />
             <AppText
               text="Get your favourite food delivered in a flash. You'll see when your rider's pick up your order, and be able to follow them along the way. You'll get a notification when they're nearby, too."
-              fontSize={14}
+              fontSize={16}
               fontFamily={FontFamily.REGULAR}
               textAlign={'justify'}
+              color={Colors.textColor}
             />
             <View style={styles.svgsButtonContainer}>
               <TouchableOpacity style={styles.appStoreIcon}>
@@ -103,14 +51,16 @@ const LandingScreen = () => {
               </TouchableOpacity>
             </View>
           </View>
-          <Image
-            height={500}
-            width={373}
-            source={{
-              uri: Images.mapImage,
-            }}
-            style={styles.mapImage}
-          />
+          <ImageBackground
+            resizeMode="cover"
+            source={{uri: Images.mapImage}}
+            style={styles.backgroundMapImage}>
+            <Image
+              source={{uri: Images.notificationImage}}
+              height={110}
+              width={300}
+            />
+          </ImageBackground>
         </View>
       </View>
       <View style={styles.discountSection}>
@@ -167,6 +117,9 @@ const styles = StyleSheet.create({
     fontSize: 38,
     fontWeight: 'bold',
   },
+  partnersImageContainer: {
+    marginBottom: 20,
+  },
   card: {
     backgroundColor: Colors.backgroundPrimary,
     borderRadius: 5,
@@ -186,67 +139,28 @@ const styles = StyleSheet.create({
     marginHorizontal: 10,
     marginVertical: 10,
   },
-  searchComponentText2: {
-    flexDirection: 'row',
-  },
+
   logInText: {
     color: Colors.eatMeColor,
   },
-  sendIcon: {
-    marginTop: 10,
-    marginLeft: 10,
-  },
+
   mapImage: {
     borderBottomLeftRadius: 5,
     borderBottomRightRadius: 5,
   },
-  inputText: {
-    flex: 2,
-    backgroundColor: Colors.backgroundPrimary,
-    height: 40,
-    paddingTop: 10,
-    paddingRight: 10,
-    paddingLeft: 15,
-    borderRadius: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  searchButton: {
-    alignItems: 'center',
-    backgroundColor: Colors.eatMeColor,
-    borderRadius: 100,
-  },
-  searchButtonComponent: {
-    flexDirection: 'row',
-    backgroundColor: Colors.backgroundPrimary,
-    borderWidth: 0.25,
-    padding: 5,
-    borderRadius: 50,
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 2,
-  },
-  searchBar: {
-    marginTop: 10,
-    marginBottom: 10,
-  },
-  searchButtonText: {
-    color: Colors.backgroundPrimary,
-    fontSize: 16,
-    fontWeight: 'bold',
-    margin: 8,
-  },
+
   svgsButtonContainer: {
     flexDirection: 'row',
-    justifyContent: 'space-evenly',
+    justifyContent: 'flex-start',
   },
   appStoreIcon: {
     marginTop: 5.45,
     marginRight: 10,
+  },
+  backgroundMapImage: {
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    height: 450,
   },
   discountSection: {
     backgroundColor: Colors.eatMeColor,
