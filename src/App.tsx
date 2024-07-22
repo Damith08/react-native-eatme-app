1cbc1846cdf24211923078ddc0294fd023759164
Damith08 <dmthsamarasinghe@gmail.com>
2024-07-22 14:52:55 +0530

    feat: add redux persist to auth

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,18 @@ import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import RootStackNavigator from './navigation/RootStackNavigator';
 import {Provider} from 'react-redux';
-import store from './store';
+import {store, persistor} from './store';
+import {PersistGate} from 'redux-persist/integration/react';
 
 function App(): React.JSX.Element {
   return (
     <Provider store={store}>
-      <StatusBar barStyle="light-content" />
-      <NavigationContainer>
-        <RootStackNavigator />
-      </NavigationContainer>
+      <PersistGate loading={null} persistor={persistor}>
+        <StatusBar barStyle="light-content" />
+        <NavigationContainer>
+          <RootStackNavigator />
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 }
